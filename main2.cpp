7a107cb69540581e697bfdbc149e01b298216c91
Rauan <rauan_pires@riseup.net>
2018-10-21 01:06:36 -0300

    Show!

--- main2.cpp
+++ main2.cpp
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 {
 	VideoCapture capture(0);
 	namedWindow("frame", WINDOW_KEEPRATIO);
-	// namedWindow("otsu", WINDOW_KEEPRATIO);
+	namedWindow("otsu", WINDOW_KEEPRATIO);
 	// namedWindow("diff", WINDOW_KEEPRATIO);
 	if(!capture.isOpened()) //Check if success
 		return -1;
@@ -29,12 +29,15 @@ int main(int argc, char* argv[])
 	Mat T1 = imread("./T1.jpg", IMREAD_GRAYSCALE);
 	Mat T2 = imread("./T2.jpg", IMREAD_GRAYSCALE);
 	Mat T3 = imread("./T3.jpg", IMREAD_GRAYSCALE);
+	Mat T4 = imread("./T4.jpg", IMREAD_GRAYSCALE);
 
-	int numImgsToLook = 7; // Melhorar isso -  strongtyped fica feio
+	int numImgsToLook = 8; // Melhorar isso -  strongtyped fica feio
 	//------------------------//
 
 	int fontSize = 2;
 	int fontThickness = 2;
+	int cropSize = 100; //escala da figura pra comparar
+	int erroMax = cropSize*cropSize*255; // erro maximo quando comparando imgs (absdiff)
 
 	for(;;){
 		
@@ -53,22 +56,23 @@ int main(int argc, char* argv[])
 		}
 
 		
-		threshold(gray, gray, 30, 255, cv::THRESH_BINARY | THRESH_OTSU);
-		// imshow("otsu",gray);
+		threshold(gray, gray, 100, 255, cv::THRESH_BINARY | THRESH_OTSU);
+		imshow("otsu",gray);
 
 		vector<vector< Point > >conts;
 		// vector<Mat> cropped;
-
-		findContours(gray, conts, RETR_LIST  , CHAIN_APPROX_NONE);
+		vector<Vec4i> hierarchy;
+		findContours(gray, conts,hierarchy, RETR_TREE  , CHAIN_APPROX_NONE);
 		unsigned int numberOfObjects=0;
 
 		vector<RotatedRect> minRect( conts.size() );
 		for(int i=0; i<conts.size(); i++)
 		{
-			if(conts.at(i).size() > 150 && conts.at(i).size() < 2000)
+			//drawContours(frame, conts, i, Scalar(0,0,255), 2, 8); //desenha contorno
+			if(conts.at(i).size() > 200 && conts.at(i).size() < 2000)
 			{
-				//drawContours(frame, conts, i, Scalar(0,0,255), 2, 8); //desenha contorno
-				numberOfObjects++;
+				
+				//numberOfObjects++;
 
 
 				// Bounding box pra fazer o footprint - Gira junto
@@ -88,16 +92,10 @@ int main(int argc, char* argv[])
         		M = getRotationMatrix2D(minRect[i].center, angle, 1.0);
 				
 				warpAffine(gray, rotated, M, gray.size(), INTER_CUBIC);
-				//warpAffine(frame, rotated, M, frame.size(), INTER_LINEAR);
+				//warpAffine(frame, rotated, M, frame.size(), INTER_CUBIC);
 				getRectSubPix(rotated, rect_size, minRect[i].center, cropped);
 
 				Point2f rect_points[4]; minRect[i].points( rect_points );
-
-				// desenha bunding boxes
-				for( int j = 0; j < 4; j++ ){
-					line( frame, rect_points[j], rect_points[(j+1)%4], Scalar(255,0,0), 4, 8 );
-					
-				}
 
 				//-- Pra pegar os itens da tela - meio gambi
 				// resize(cropped, cropped, Size(100, 100), 0, 0, INTER_CUBIC); 
@@ -148,57 +146,78 @@ int main(int argc, char* argv[])
 				s = cv::sum( diff )[0];
 				if(s < min){
 					min = s;
-					minShape = 6;
+					minShape = 5;
 				}
 				absdiff(cropped, T3, diff);
 				s = cv::sum( diff )[0];
 				if(s < min){
 					min = s;
-					minShape = 7;
-				} 
+					minShape = 5;
+				}
+				absdiff(cropped, T4, diff);
+				s = cv::sum( diff )[0];
+				if(s < min){
+					min = s;
+					minShape = 5;
+				}  
 
+				 // Se identificou como triangulo e tem um contorno filho, Deve ser A, pois triangulo nao tem filho..(esteril ?)
+				if(minShape == 5 && hierarchy[i][2] != -1){
+					minShape = 1;
+					//recalcula o fitness pq foi sobreescrito pelo do triangulo, que apesar de melhor, nao estava correto
+					absdiff(cropped, A, diff);
+					min = cv::sum( diff )[0];
+				}
 
 				float fitness = 0;
-				fitness = 1 - ((1*min)/(cropSize*cropSize*255));
+				fitness = 1 - ((1*min)/(erroMax));
 
-				if(fitness < 0.65){ // Se o fitness nao for bom, figura nao identificada
+				Rect boundRect = boundingRect(Mat(conts[i]));
+				// Se o fitness nao for bom, figura nao identificada
+				// Poderia estimar um fitness bom pra cada figura. A pode ser identificado com menos por exemplo
+				if(fitness < 0.65){ 
 					minShape = 0;
+				}else{
+
+					//Desenha bounding rectangle normal
+					rectangle( frame, boundRect.tl(), boundRect.br(), Scalar(0,191,255), 2, 8, 0 );
+					
+					// //Desenha bounding rectangle minimo
+					// for( int j = 0; j < 4; j++ )
+					// 	line( frame, rect_points[j], rect_points[(j+1)%4], Scalar(255,0,0), 4, 8 );
 				}
+				//Ponto pra escrever o texto
+				
+				Point pText = boundRect.tl(); 
+
 				switch(minShape){
 					case 1:
 					sprintf(str,"A (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
+					putText(frame, str, pText, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
 					break;
 					case 2:
 					sprintf(str,"B (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
+					putText(frame, str, pText, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
 
 					break;
 					case 3:
-					sprintf(str,"SPHERE (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
+					sprintf(str,"CIRCULO (%.4f)", fitness );
+					putText(frame, str,pText, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
 
 					break;
 					case 4:
-					sprintf(str,"SQUARE (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
+					sprintf(str,"QUADRADO (%.4f)", fitness );
+					putText(frame, str, pText, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
 
 					break;
 					case 5:
-					sprintf(str,"TRIANGLE (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
+					sprintf(str,"TRIANGULO (%.4f)", fitness );
+					putText(frame, str, pText, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
 					break;
-					case 6:
-					sprintf(str,"TRIANGLE (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
-					break;
-					case 7:
-					sprintf(str,"TRIANGLE (%.4f)", fitness );
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
-					break;
+
 					default:
-					sprintf(str,"?");
-					putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
+					// sprintf(str,"?");
+					// putText(frame, str, minRect[i].center, FONT_HERSHEY_PLAIN,fontSize, Scalar(0,255,100),fontThickness);
 					break;
 				}
 
@@ -207,10 +226,6 @@ int main(int argc, char* argv[])
 		}
 
 		imshow("frame",frame);
-
-
-
-
 		
 		if(waitKey(30) >= 0) break;
 	}
